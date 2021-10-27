7f01425b3313cebbeac57d7d70bc2b19e9c71a67
ausbitbank <ausbitbank@gmail.com>
2021-10-27 15:11:09 +1000

    recognise HBD transfers

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,7 +10,7 @@
       </transition>
       <transition appear :enter-active-class="anim1" :leave-active-class="anim2">
       <div class="text-title bg-black q-ma-sm q-pa-sm" style="font-size: 50px">
-        {{ alert.user }} sent {{ alert.amount.split(' ')[0] }} <q-avatar size="md" class="q-ma-xs"><img src="https://hive.ausbit.dev/statics/hive.svg" /></q-avatar>
+        {{ alert.user }} sent {{ alert.amount.split(' ')[0] }} <q-avatar size="md" class="q-ma-xs"><img :src="alert.amount.split(' ')[1] === 'HIVE' ? 'https://hive.ausbit.dev/statics/hive.svg' : 'https://hive.ausbit.dev/statics/hbd.svg'" /></q-avatar>
       </div>
       </transition>
       <transition appear :enter-active-class="anim1" :leave-active-class="anim2">
@@ -49,6 +49,7 @@
       </q-btn><br />
       <q-btn label="Test text to speech" @click="textToSpeech('Testing text to speech, ermagerd')" color="orange" icon="tty" v-if="false" />
       <q-btn label="test dono TTS" @click="newDonation(alert.user,alert.amount,alert.memo)" color="orange" icon="tty" />
+      <div><a href="https://github.com/ausbitbank/donationalerts"><q-icon name="code" /> github.com/ausbitbank/donationalerts</a></div>
     </div>
   </q-page>
 </template>
@@ -95,7 +96,7 @@ export default defineComponent({
         title: 'Donation!',
         user: 'ausbitbank',
         memo: 'Great stream keep it up!',
-        amount: '10'
+        amount: '10 HIVE'
       },
       transferHistory: {
         current: [],
@@ -108,7 +109,7 @@ export default defineComponent({
       },
       async getTransfers () {
         this.timerStarted = true
-        var h = await client.call('condenser_api', 'get_account_history', [this.username, -1, 5, "4", null]) // Get transfer operations only
+        var h = await client.call('condenser_api', 'get_account_history', [this.username, -1, 5, "4", null]) // Get last 5 transfer operations only
         if (this.transferHistory.latest !== null) { // Not the first loop
           if (h[h.length - 1][0] > this.transferHistory.latest[0]) { // If the last saved txid is different to the most recent
             console.log('new transfers')
@@ -116,9 +117,10 @@ export default defineComponent({
               var id = i[0]
               var op = i[1].op[1]
               var amount = parseInt(op.amount.split(' ')[0])
+              var token = parseInt(op.amount.split(' ')[1])
               if (!this.alreadyPlayedAlerts.includes(id)) { // We haven't seen this alert already
                 if (id > this.transferHistory.latest[0]) { // If ID is greater then the last known transfer
-                  if (op.to === this.username && !ignoreAccounts.split(',').includes(op.from)) {// And is an incoming transfer, not from an ignored account
+                  if (op.to === this.username && !this.ignoreAccounts.split(',').includes(op.from)) {// And is an incoming transfer, not from an ignored account
                     if (amount >= this.minAlert) { // Amount is above minimum for alert
                       console.log(op)
                       this.playSound()
@@ -129,10 +131,7 @@ export default defineComponent({
                       if (amount >= this.minMemo && op.memo !== '') { // Amount is above minimum for memo
                         this.alert.memo = op.memo
                         if (amount >= this.minTTS) { // Amount is above minimum for tts
-                          var tts = this.alert.user + ' sent ' + amount + ' hive ... ' + this.alert.memo
-                          console.log('Play tts: "' + tts + '"')
-                          this.textToSpeech(tts)
-                        }
+                          this.newDonation(this.alert.user, this.alert.amount, this.alert.memo)                        }
                       }
                       this.alreadyPlayedAlerts.push(id)
                       this.hideAlert()
@@ -151,9 +150,7 @@ export default defineComponent({
           h.forEach( i => { this.alreadyPlayedAlerts.push(i[0]) })
         }
       },
-      delay(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms))
-      },
+      delay(ms) { return new Promise(resolve => setTimeout(resolve, ms)) },
       async hideAlert() {
         await this.delay(this.alertDelay)
         this.showAlert = false
@@ -168,12 +165,10 @@ export default defineComponent({
       },
       newDonation(username, amount, memo) {
         this.showAlert = true
-        this.textToSpeech(username + ' sent ' + amount + ' hive')
+        this.textToSpeech(username + ' sent ' + amount)
         this.textToSpeech(memo)
       },
-      textToSpeech(what) {
-        window.speechSynthesis.speak(new SpeechSynthesisUtterance(what))
-      }
+      textToSpeech(what) { window.speechSynthesis.speak(new SpeechSynthesisUtterance(what)) }
     }
   },
   computed: {
